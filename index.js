f489b12796171ac9ced78b8837f82ad88701b003
Raymond Mojica <rm@M12020.home>
2022-05-06 23:27:30 -0400

    added newItem function

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-function newImage(source){
-    let name = document.createElement('img')
-    name.src = source
-    name.style.position = 'fixed'
-    name.style.left = '100px'
-    name.style.bottom = '100px'
-    document.body.append(name)
-    return name
+function newImage(source, positionLeft, positionBottom){
+    let name = document.createElement('img');
+    name.src = source;
+    name.style.position = 'fixed';
+    name.style.left = positionLeft;
+    name.style.bottom = positionBottom;
+    document.body.append(name);
+    return name;
 
+}
+
+function newItem(source, positionLeft, positionBottom){
+    let item = document.createElement('img')
+    item.src = source
+    item.style.position = 'fixed'
+    item.style.left = positionLeft
+    item.style.bottom = positionBottom
+    document.body.append(item)
+    item.addEventListener('click', function(){
+    item.remove()
+})
 }
 
 /*let greenCharacter = document.createElement('img')
@@ -24,5 +36,16 @@ pinetree.style.bottom = '200px'
 document.body.append(pinetree)
 */
 
-newImage('assets/green-character.gif');
-newImage('assets/pine-tree.png');
+newImage('assets/green-character.gif', '100px', '100px');
+newImage('assets/pine-tree.png', '450px', '200px');
+newImage('assets/tree.png', '200px', '300px');
+newImage('assets/pillar.png', '350px', '100px');
+newImage('assets/crate.png', '150px', '200px');
+newImage('assets/well.png', '500px', '425px');
+newItem('assets/sword.png', '500px', '405px');
+newItem('assets/sheild.png', '165px', '185px');
+newItem('assets/staff.png', '600px', '100px');
+
+
+
+
