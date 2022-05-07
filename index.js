6ec563d1e0e72a805f7056e6bb531de83093bbc1
Raymond Mojica <rm@M12020.home>
2022-05-06 22:23:42 -0400

    Added Pine Tree

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+let pinetree = document.createElement('img')
+pinetree.src = 'assets/pine-tree.png'
+pinetree.style.position = 'fixed'
+pinetree.style.left = '450px'
+pinetree.style.bottom = '200px'
+document.body.append(pinetree)
